b8e89c087bada8d1f21a7b16252d6805ac8b1341
fengxiang22 <18602885377@163.com>
2020-06-11 10:59:40 +0800

    1

--- run_suite.py
+++ run_suite.py
@@ -7,4 +7,4 @@ from app import BASE_PATH
 
 test_suite = unittest.TestLoader().discover(start_dir=BASE_PATH + "/script/", pattern="test_ihrm_employee.py")
 file_name = "report-{}.html".format(time.strftime("%Y%m%d %H%M%S"))
-BeautifulReport(test_suite).report(filename=file_name, log_path=BASE_PATH + "/log",description="员工的增删改查")
+BeautifulReport(test_suite).report(filename=file_name, log_path=BASE_PATH + "/report",description="员工的增删改查")
